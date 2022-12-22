f024d2801ca5f900118d1049b9254af3cc8a6211
zymmiwang <101502396+zymmiwang@users.noreply.github.com>
2022-12-22 15:55:38 +0800

    fix-jian&fan-1
    修复简繁互化因特殊字符而报错终止的错误

--- python/fantojian.py
+++ python/fantojian.py
@@ -5,6 +5,8 @@ import fileinput
 
 def fantojian(hant_str: str):
     return zhconv.convert(hant_str, 'zh-hans')
+    
+
 
 
 filename=sys.argv[1]
@@ -16,18 +18,43 @@ try:
     f = open(filename,'r')
     line = f.readline()               # 调用文件的 readline()方法，一次读取一行
     with open(outname,'a') as fe:
-        while line:   
-            fe.writelines(fantojian(line))                  
-            line = f.readline()
+        while line:
+            try:
+                a=fantojian(line)                
+                fe.writelines(fantojian(a))                  
+                line = f.readline()  
+            except:#过滤特殊字符
+                for i in range(0,len(line)-1):
+                    try:
+                        line[i]=fantojian(str(line[i]))
+                    except:
+                        line[i]=line[i]
+                        
+                fe.writelines(line)                  
+                line = f.readline()  
+                 
 
-            
+
+ #utf-8           
 except UnicodeDecodeError:
     f = open(filename,'r',encoding="utf-8")
     line = f.readline()               # 调用文件的 readline()方法，一次读取一行
-    with open(outname,'a') as fe:
-        while line:   
-            fe.writelines(fantojian(line))                   
-            line = f.readline()
+    with open(outname,'a',encoding="utf-8") as fe:
+        while line:
+            try:
+                a=fantojian(line)                
+                fe.writelines(fantojian(a))                  
+                line = f.readline()  
+            except:#过滤特殊字符
+                for i in range(0,len(line)-1):
+                    try:
+                        line[i]=fantojian(str(line[i]))
+                    except:
+                        line[i]=line[i]
+                fe.writelines(line)                  
+                line = f.readline()  
+
+            
     
 
 with open(outname,'a') as fe:
--- python/jiantofan.py
+++ python/jiantofan.py
@@ -17,22 +17,47 @@ try:
     f = open(filename,'r')
     line = f.readline()               # 调用文件的 readline()方法，一次读取一行
     with open(outname,'a') as fe:
-        while line:   
-            fe.writelines(jiantofan(line))                  
-            line = f.readline()
+        while line:
+            try:
+                a=jiantofan(line)                
+                fe.writelines(jiantofan(a))                  
+                line = f.readline()  
+            except:#过滤特殊字符
+                for i in range(0,len(line)-1):
+                    try:
+                        line[i]=jiantofan(str(line[i]))
+                    except:
+                        line[i]=line[i]
+                fe.writelines(line)                  
+                line = f.readline()  
+                 
 
-            
+
+ #utf-8           
 except UnicodeDecodeError:
     f = open(filename,'r',encoding="utf-8")
     line = f.readline()               # 调用文件的 readline()方法，一次读取一行
-    with open(outname,'a') as fe:
-        while line:   
-            fe.writelines(jiantofan(line))                   
-            line = f.readline()           
+    with open(outname,'a',encoding="utf-8") as fe:
+        while line:
+            try:
+                a=jiantofan(line)                
+                fe.writelines(jiantofan(a))                  
+                line = f.readline()  
+            except:#过滤特殊字符
+                for i in range(0,len(line)-1):
+                    try:
+                        line[i]=jiantofan(str(line[i]))
+                    except:
+                        line[i]=line[i]
+                fe.writelines(line)                  
+                line = f.readline()  
+
+            
+    
 
 with open(outname,'a') as fe:
     while line:   
-        fe.writelines(jiantofan(line))                   # 后面跟 ',' 将忽略换行符   
+        fe.writelines(jiantofan(line))                 
         line = f.readline()
 
 f.close()
